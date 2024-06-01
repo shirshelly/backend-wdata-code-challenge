dcebd5fb6d50cb458c68e76f335400cdb5673809
shirshelly <shirshelly1@gmail.com>
2024-06-01 19:20:05 +0300

    initial commit, digest data and create API

---
+++ database.py
@@ -0,0 +1,38 @@
+import os
+import sqlite3
+import pandas as pd
+
+db_file = 'weather_data.db'
+def convert_inch_to_mm(df):
+    df['Precipitation Rate in/hr'] = df['Precipitation Rate in/hr'] * 25.4
+    df.rename(columns={'Precipitation Rate in/hr': 'Precipitation Rate mm/hr'}, inplace=True)
+
+
+def convert_fahrenheit_to_celsius(df):
+    df['Temperature Fahrenheit'] = (df['Temperature Fahrenheit'] - 32) * (5 / 9)
+    df.rename(columns={'Temperature Fahrenheit': 'Temperature Celsius'}, inplace=True)
+
+
+def ingest_data():
+    # Delete the file if it exists
+    if os.path.exists(db_file):
+        os.remove(db_file)
+    connection = sqlite3.connect(db_file)
+    data_folder = "./data"
+    for data_file in os.listdir(data_folder):
+        df = pd.read_csv(data_folder + '\\' + data_file)
+        if "Precipitation Rate in/hr" in df:
+            convert_inch_to_mm(df)
+        if "Temperature Fahrenheit" in df:
+            convert_fahrenheit_to_celsius(df)
+        df.to_sql('weather', connection, if_exists="append")
+    connection.close()
+
+
+def query(lon, lat):
+    connection = sqlite3.connect(db_file)
+    c = connection.cursor()
+    c.execute("SELECT * FROM weather WHERE Longitude=" + lon + " AND Latitude=" + lat)
+    records = c.fetchall()
+    connection.close()
+    return records
---
+++ main.py
@@ -0,0 +1,38 @@
+from flask import Flask, request
+from database import ingest_data, query
+import json
+
+app = Flask(__name__)
+
+
+def check_condition(temperature, precipitation, condition):
+    if condition == "veryHot" and float(temperature) > 30:
+        return True
+    if condition == "rainyAndCold" and float(temperature) < 10 and float(precipitation) > 0.5:
+        return True
+    return False
+
+
+@app.route('/weather/insight')
+def get_data():
+    lon = request.args.get("lon")
+    lat = request.args.get("lat")
+    condition = request.args.get("condition")
+
+    if lon and lat and condition:
+        records = query(lon, lat)
+        results = []
+        for record in records:
+            forcast_time = record[3]
+            temperature = record[4]
+            precipitation = record[5]
+            condition_met = check_condition(temperature, precipitation, condition)
+            results.append({"forecastTime": forcast_time, "conditionMet": condition_met})
+
+        return json.dumps(results, indent=2), 200
+    return 'error', 404
+
+
+if __name__ == '__main__':
+    ingest_data()
+    app.run()
